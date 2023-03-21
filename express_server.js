1312c15426b4d5a8492b5d84dbb6579c195f379d
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-03-20 21:07:17 -0400

    server with post

--- express_server.js
+++ express_server.js
@@ -5,6 +5,8 @@ const PORT = 8080; // default port
 
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -28,3 +30,18 @@ app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+function generateRandomString() {
+  const randOptions = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'; // full set of 62 alphanumeric characters to select from
+  let randString = ''; // randomized string to be returned
+  for (let i = 0; i < 6; i ++) {
+    const randomizer = Math.floor(Math.random() * 62); //random number between 0 and 62
+    randString += randOptions[randomizer];
+  }
+  return randString;
+};
